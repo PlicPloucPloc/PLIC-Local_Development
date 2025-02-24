5f938df747b89155f778928cc58a17cb91c45179
Baptiste Perrier <baptiste.perrier@epita.fr>
2025-02-25 00:40:35 +0100

    Add supabase image

---
+++ supabase-install.sh
@@ -0,0 +1,11 @@
+# Go to the docker folder
+cd supabase/docker
+
+# Copy the fake env vars
+cp .env.example .env
+
+# Pull the latest images
+docker compose pull
+
+# Start the services (in detached mode)
+docker compose up -d
