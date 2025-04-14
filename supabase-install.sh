10f868ce15d9f2db92a2b1fbaea4a0cb7a179ff9
Babouche007 <baptiste.perrier@epita.fr>
2025-04-14 12:08:57 +0200

    fix: correct supabase start config

--- supabase-install.sh
+++ supabase-install.sh
@@ -1,4 +1,8 @@
+#Clone the Supabase repository
+git clone --depth 1 https://github.com/supabase/supabase
+#Copy the .env file to the docker directory
 cp .env supabase/docker/.env
+# Navigate to the docker directory
 cd supabase/docker
 # Pull the latest images
 docker compose pull
