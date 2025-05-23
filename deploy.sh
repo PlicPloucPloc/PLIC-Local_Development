2964286a9957e0cd9247902545bd12429ffc0e17
Babouche007 <baptiste.perrier@epita.fr>
2025-05-23 16:59:22 +0200

    Fix conflict

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo Input resend API key:
 
 read Resend_Key
 
-sed -i "/\[auth.email.smtp\]/,/^\[/ s/\(pass = \"\)[^\"]*/\1$Resend_Key/" supabase/config.toml
+sed -i "/\[auth.email.smtp\]/,/^\[/s/\(pass = \"\)[^\"]*/\1$Resend_Key/" supabase/config.toml
 
 npx supabase start
