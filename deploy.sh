704273a10c519d5d9d9ed1adfa3ccc2fd53c0399
Ellmos <95100773+Ellmos@users.noreply.github.com>
2025-06-24 13:13:54 +0200

    feat: added local development gateway (#7)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo Input resend API key: 
 read Resend_Key
 
-sed -i "s/\[auth.email.smtp\]/,/^\[/s/\(pass = \"\)[^\"]*/\1$Resend_Key/g" supabase/config.toml
+sed -i "/\[auth.email.smtp\]/,/^\[/s/\(pass = \"\)[^\"]*/\1$Resend_Key/" supabase/config.toml
 tmp=$(sed -E -e "s/service_role key: ([^\n]*)/\1/gm;t;d" <<< $(npx supabase start))
 
 echo $tmp
