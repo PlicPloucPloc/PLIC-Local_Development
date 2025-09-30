99f86a3805074c45c0c9a24e81d3e088df6d32fb
Babouche007 <baptiste.perrier@epita.fr>
2025-09-30 17:04:38 +0200

    Add db migration

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ read Resend_Key
 
 sed -i "/\[auth.email.smtp\]/,/^\[/s/\(pass = \"\)[^\"]*/\1$Resend_Key/" supabase/config.toml
 tmp=$(sed -E -e "s/service_role key: ([^\n]*)/\1/gm;t;d" <<< $(npx supabase@2.26.9 start))
+npx supabase@2.26.9 db reset
 
 echo $tmp
 
