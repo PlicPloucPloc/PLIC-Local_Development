978687190198728f41862a864f0e9e72e9ca7577
Babouche <42094854+Babouche007@users.noreply.github.com>
2025-10-20 21:24:09 +0200

    Update deploy script to extract role and JWT keys

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,18 @@ read Resend_Key
 cp .env.example .env
 
 sed -i "/\[auth.email.smtp\]/,/^\[/s/\(pass = \"\)[^\"]*/\1$Resend_Key/" supabase/config.toml
-tmp=$(sed -E -e "s/service_role key: ([^\n]*)/\1/gm;t;d" <<< $(npx supabase@2.26.9 start))
+sup_config=$(npx supabase@2.26.9 start)
+role_key=$(sed -E -e "s/service_role key: ([^\n]*)/\1/gm;t;d" <<< sup_config)
+jwt_key=$(sed -E -e "s/JWT secret: ([^\n]*)/\1/gm;t;d" <<< sup_config)
 
 echo $test
 
 npx supabase@2.26.9 db reset
 
-echo $tmp
+echo $role_key
+echo $jwt_key
 
-sed -i "/(SERVICE_ROLE_KEY=)[^\n]*/\1$tmp/" .env
+sed -i "/(SERVICE_ROLE_KEY=)[^\n]*/\1$role_key/" .env
+sed -i "/(JWT_SECRET=)[^\n]*/\1$jwt_key/" .env
 docker compose build
 docker compose up -d
