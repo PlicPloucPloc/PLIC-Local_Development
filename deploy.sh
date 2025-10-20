076a1d621fb83e89c6b41515effd75fbef4d208c
Babouche007 <baptiste.perrier@epita.fr>
2025-10-20 21:57:54 +0200

    fix chat config

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,13 @@ cp .env.example .env
 sed -i "/\[auth.email.smtp\]/,/^\[/s/\(pass = \"\)[^\"]*/\1$Resend_Key/" supabase/config.toml
 sup_config=$(npx supabase@2.26.9 start)
 role_key=$(sed -E -e "s/service_role key: ([^\n]*)/\1/gm;t;d" <<< $sup_config)
-jwt_key=$(sed -E -e "s/JWT secret: ([^\n]*)/\1/gm;t;d" <<< $sup_config)
 
 echo $test
 
 npx supabase@2.26.9 db reset
 
 echo $role_key
-echo $jwt_key
 
-sed -i "/(SERVICE_ROLE_KEY=)[^\n]*/\1$role_key/" .env
-sed -i "/(JWT_SECRET=)[^\n]*/\1$jwt_key/" .env
+sed -i -E 's/(SERVICE_ROLE_KEY=)[^\n]*/\1'$role_key'/gm;t' .env
 docker compose build
 docker compose up -d
