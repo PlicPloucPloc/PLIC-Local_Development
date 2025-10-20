3844702353b9b982774070d4c9686710304a123d
Babouche <42094854+Babouche007@users.noreply.github.com>
2025-10-20 21:24:41 +0200

    Fix variable assignment for role_key and jwt_key

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ cp .env.example .env
 
 sed -i "/\[auth.email.smtp\]/,/^\[/s/\(pass = \"\)[^\"]*/\1$Resend_Key/" supabase/config.toml
 sup_config=$(npx supabase@2.26.9 start)
-role_key=$(sed -E -e "s/service_role key: ([^\n]*)/\1/gm;t;d" <<< sup_config)
-jwt_key=$(sed -E -e "s/JWT secret: ([^\n]*)/\1/gm;t;d" <<< sup_config)
+role_key=$(sed -E -e "s/service_role key: ([^\n]*)/\1/gm;t;d" <<< $sup_config)
+jwt_key=$(sed -E -e "s/JWT secret: ([^\n]*)/\1/gm;t;d" <<< $sup_config)
 
 echo $test
 
